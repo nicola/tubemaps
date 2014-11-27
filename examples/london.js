0f7cf501a25012abbeb1e87132da38bce9fdf118
nicola <notsecurity@gmail.com>
2014-11-27 13:54:32 +0000

    adding support to JSON file, no need to parse CSV

--- examples/london.js
+++ examples/london.js
@@ -1,22 +1,9 @@
 var TubeMap = require('../').TubeMap;
 var readCSVs = require('../').readCSVs;
+var Maps = require('../maps');
 
-var files = {
-  connections: __dirname + '/../datasets/london.connections.csv',
-  lines: __dirname + '/../datasets/london.lines.csv',
-  stations: __dirname + '/../datasets/london.stations.csv'
-};
+Maps('london', function(err, tube) {
 
-readCSVs(files, function(err, csvs) {
-
-  var tubemap = new TubeMap({
-    connections: csvs[0],
-    lines: csvs[1],
-    stations: csvs[2]
-  });
-
-  // How many connections does Victoria station have?
-  var victoria = tubemap.getStationByName('Victoria');
-  console.log(victoria.conns.length / 2)
+  console.log(tube.getStationByName('Victoria'));
 
 });
--- maps.js
+++ maps.js
@@ -1,26 +1,8 @@
 var TubeMap = require('./index').TubeMap;
 var readCSVs = require('./index').readCSVs;
-
-var files = {
-  "london": {
-    connections: __dirname + '/datasets/london.connections.csv',
-    lines: __dirname + '/datasets/london.lines.csv',
-    stations: __dirname + '/datasets/london.stations.csv'
-  }
-};
+var datasets = require('./datasets');
 
 module.exports = function(city, cb) {
-  readCSVs(files[city], function(err, csvs) {
-
-    cb(
-      err,
-      new TubeMap({
-        connections: csvs[0],
-        lines: csvs[1],
-        stations: csvs[2]
-      })
-    );
-
-  });
-
+  var dataset = require(datasets[city].json);
+  cb(null, new TubeMap(dataset));
 };
